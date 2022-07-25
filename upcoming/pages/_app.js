d0a695c35e251b8eb5030138a8d44ee133c6313e
Ph0-ever <oluwayomiforever@gmail.com>
2022-07-25 11:17:18 +0100

    feat: landing page responsiveness

--- upcoming/pages/_app.js
+++ upcoming/pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
+import '../public/css/responsive-styles.css'
 import '../public/css/styles.css'
+import '../components/util/breakpoints.js'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
