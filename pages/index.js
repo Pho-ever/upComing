1b122a779024829635a60939a46b5efadd73cb8c
Ph0-ever <oluwayomiforever@gmail.com>
2022-07-27 10:54:58 +0100

    fix: image

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Navbar from '../components/navbar'
 import Landing from '../components/landing'
 import Swiper from '../components/swiper'
+// import Swiper from ''
 
 
 export default function Home() {
   return (
     <div className="container">
+      {/* <img src="../public/images/NG-netflix.jpg" /> */}
       <Navbar />
       <Landing />
       <Swiper />
