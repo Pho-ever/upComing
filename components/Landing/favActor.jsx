58f4153671ced4de20f5ab08f9168ab9ba0ddb45
Ph0-ever <oluwayomiforever@gmail.com>
2022-08-03 13:47:09 +0100

    feat: landing page

--- components/Landing/favActor.jsx
+++ components/Landing/favActor.jsx
@@ -1,8 +1,13 @@
 const FavActor = () => {
     return (
         <div className="favactor-container">
-            <h2 className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus veritatis impedit distinctio nesciunt mollitia, voluptas, veniam odit quis pariatur repellat, ut unde. Aperiam cum temporibus similique voluptatibus, possimus perferendis dignissimos.</h2>
-            <h2 className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga minus ea optio illum aliquam similique dignissimos consequatur neque voluptatem magnam quod quidem laudantium fugiat beatae delectus excepturi iure, at ipsam.</h2>
+                <div>
+                    <h2 className="">Lorem ipsum dolor </h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium placeat beatae eligendi consectetur voluptatibus fugiat voluptas nostrum debitis. Totam saepe voluptatem enim debitis aliquid quos.</p>
+                </div>
+                <div>
+                    <img className="favactor-img" src="enders.jpg" alt="logo" />
+                </div>
         </div>
     );
 }
