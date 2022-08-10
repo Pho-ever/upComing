de71707c5232291e30c87cb1566f90a5db76bd63
Ph0-ever <oluwayomiforever@gmail.com>
2022-08-10 17:55:13 +0100

    chore: used an array for the buttons

--- components/Landing/Questions.jsx
+++ components/Landing/Questions.jsx
@@ -1,25 +1,49 @@
-import {useState} from 'react'
+// import {useState} from 'react'
 
 const Questions = () => {
-    const [dropdown, setIsDropdown] = useState(false)
+    // const [dropdown, setIsDropdown] = useState(false)
 
-    const handleDropdown = () => {
-        setIsDropdown(!dropdown)
-    }
+    // const handleDropdown = (index) => {
+    //     setIsDropdown(!dropdown)
+    // }
+
+    const FAQ = [
+        {heading: 'What is Upcoming', content: ""},
+        {heading: 'How much does UpComing cost?', content:""},
+        {heading: 'Can I stream on Upcoming?', content:""},
+        {heading: 'Can I get notifications?', content:""}
+    ]
+        
+    
 
     return ( 
         <>
             <div className="faq-container">
                 <h1>Frequently Asked Questions</h1>
-                <button onClick={handleDropdown}>What is Upcoming? </button>
-
+                    {FAQ.map(item => (<button>
+                        {item.heading}
+                    </button>))}
+                    
+                
+                {/* <button onClick={handleDropdown}>What is Upcoming? </button>
                 {dropdown && <div className="faq-dropdown">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quis quae laborum quas, impedit corrupti excepturi voluptates accusantium quam culpa natus voluptatibus iusto quia perferendis eum, odit vel labore qui est quo ipsam! Unde non explicabo, debitis veniam tenetur natus.
                     </div>}
 
-                <button>How much does Upcoming cost?</button>
-                <button>Can I stream on Upcoming? </button>
-                <button>Can I get notifications? </button>
+                <button onClick={handleDropdown}>How much does Upcoming cost?</button>
+                {dropdown && <div className="faq-dropdown">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quis quae laborum quas, impedit corrupti excepturi voluptates accusantium quam culpa natus voluptatibus iusto quia perferendis eum, odit vel labore qui est quo ipsam! Unde non explicabo, debitis veniam tenetur natus.
+                    </div>}
+
+                <button onClick={handleDropdown}>Can I stream on Upcoming? </button>
+                {dropdown && <div className="faq-dropdown">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quis quae laborum quas, impedit corrupti excepturi voluptates accusantium quam culpa natus voluptatibus iusto quia perferendis eum, odit vel labore qui est quo ipsam! Unde non explicabo, debitis veniam tenetur natus.
+                    </div>}
+
+                <button onClick={handleDropdown}>Can I get notifications? </button>
+                {dropdown && <div className="faq-dropdown">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quis quae laborum quas, impedit corrupti excepturi voluptates accusantium quam culpa natus voluptatibus iusto quia perferendis eum, odit vel labore qui est quo ipsam! Unde non explicabo, debitis veniam tenetur natus.
+                    </div>} */}
             </div>
         </>
     );
